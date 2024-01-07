dc8be9945b4efc060229a30fd5c50dcdd44f6e39
Vladuslav Dragushin <maxhc@inbox.ru>
2024-01-07 19:12:04 +0300

    Added database_config

--- socket_app/orm/database.py
+++ socket_app/orm/database.py
@@ -4,10 +4,9 @@ from sqlalchemy import create_engine
 
 engine = create_engine("sqlite:///db.sqlite3")
 
-session = sessionmaker(
+session_maker = sessionmaker(
     engine,
     expire_on_commit=False,
-    echo=True,
 )
 
 Base = declarative_base()
